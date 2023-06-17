5385f3da89228b7ed171bdb3a8f49da56a8c6d47
Pranesh Kumar <66989022+praneshkumar-12@users.noreply.github.com>
2023-06-17 12:49:55 +0530

    Added  csrf trusted origins

--- ClinicManagementSystemProduction/settings.py
+++ ClinicManagementSystemProduction/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
+CSRF_TRUSTED_ORIGINS = ["https://cms-django.azurewebsites.net", "https://www.cms-django.azurewebsites.net"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
